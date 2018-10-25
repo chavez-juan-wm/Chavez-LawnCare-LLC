5681cfdcc4530e8f76f74c677d4b9d6a80fcc33d
Manny Chavez <manny.chavez12@yahoo.com>
2018-10-24 19:44:35 -0700

    Removed images, added a map, and updated the css/javascript

--- js/main.js
+++ js/main.js
@@ -17,9 +17,9 @@ function main() {
             }
           });
 
-        // Show Menu on Book
+        // Show logo on navbar
         $(window).bind('scroll', function() {
-            var navHeight = $(window).height() - 350;
+            var navHeight = $(window).height() - 400;
             if ($(window).scrollTop() > navHeight)
             {
                 $('#logo').fadeIn("slow");
@@ -44,91 +44,34 @@ function main() {
         }
       });
 
-        // Portfolio isotope filter
-        $(window).load(function() {
-            var $container = $('.portfolio-items');
 
-            $container.isotope({
-                filter: '.slider',
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
 
-            $('.cat a').click(function() {
-                $('.cat .active').removeClass('active');
-                $(this).addClass('active');
-                var selector = $(this).attr('data-filter');
-
-                $container.isotope({
-                    filter: selector,
-                    animationOptions: {
-                        duration: 750,
-                        easing: 'linear',
-                        queue: false
-                    }
-                });
-                return false;
-            });
-
-            $('.dog a').click(function() {
-                if(!$(this).hasClass('mode')){
-                    $('.dog .mode').removeClass('mode');
-                    $(this).addClass('mode');
-                    var selector = $(this).attr('data-filter');
-
-                    if(selector === '.grid'){
-                        $("#carousel7").carousel('pause');
-                        $('.categories').fadeIn("slow");
-                    }
-                    else{
-                        $('#carousel7').carousel({
-                            interval: 4000,
-                            keyboard: true,
-                            pause: "hover"
-                        });
-
-                        $('.categories').fadeOut("fast");
-                    }
-
-                    $container.isotope({
-                        filter: selector,
-                        animationOptions: {
-                            duration: 750,
-                            easing: 'linear',
-                            queue: false
-                        }
-                    });
-                }
-                return false;
-            });
-
-        });
-
-        // Nivo Lightbox
-        $('.portfolio-item a').nivoLightbox({
-                effect: 'slideDown',
-                keyboardNav: true
-        });
-
-        // Testimonial Slider
         $(document).ready(function() {
+            // Testimonial Slider
             $('.owl-carousel').owlCarousel({
                 margin: 10,
                 loop: true,
-                nav: false,
                 items: 1
             });
 
-            $('#carousel7').carousel({
+            //Bootstrap carousel
+            $('#carousel').carousel({
                 interval: 4000,
                 keyboard: true,
                 pause: "hover"
             });
             $('.grid .carousel').carousel({
                 interval: false
+            });
+
+            //Map
+            mapboxgl.accessToken = 'pk.eyJ1IjoiY2hhdmV6Z2FyajE3NTUiLCJhIjoiY2ptaW9iOXl2MDc1MDNwcG1iam1xYWN3YiJ9.0KoeWq7s29S_wI_bPhGw2A';
+            var map = new mapboxgl.Map({
+                container: 'map',
+                style: 'mapbox://styles/mapbox/streets-v9',
+                center: [-112.426462, 33.4628339],
+                zoom: 9,
+                interactive: false
             });
 
         });
