4e90cf47d866d8ffd66a25767bd78efd364d8860
Manny Chavez <manny.chavez12@yahoo.com>
2018-10-31 21:11:54 -0700

    Removed phpmailer

--- mailer/contact_me.php
+++ mailer/contact_me.php
@@ -13,31 +13,21 @@
     $address = $_POST['address'];
     $emailUpdates = $_POST['emailUpdates'];
     $phone = $_POST['phone'];
+    $from = 'no-reply@chavezlawncare.com';
 
-    // Create the email and send the message
+    // Create the email
+    $headers  = "From: $from\r\n";
+    $headers .= "Content-type: text/html\r\n";
+    //$headers .= "Cc: email";
+
     $email_subject = "Website Contact Form:  $name";
-    $email_body = "Name: $name<br>Email: $email_address<br>Phone: $phone <br><br>Address: <br> $address <br><br>$emailUpdates<br><br>Message:<br>$message";
+    $email_body = "Name: $name<br/>Email: $email_address<br/>Phone: $phone <br/><br/>Address: <br/> $address <br/><br/>$emailUpdates<br/><br/>Message:<br/>$message";
 
-    require_once('class.phpmailer.php');
-    $mail = new PHPMailer();
-    $mail->IsSMTP();
-    $mail->SMTPDebug  = 0;
-    $mail->SMTPAuth   = true;
-    $mail->SMTPSecure = "ssl";
-    $mail->Host       = "smtp.gmail.com";
-    $mail->Port       = 465;
-    $mail->AddAddress('manny.chavez12@yahoo.com');
-//    $mail->AddAddress('omar@chavezlawncare.com');
-//    $mail->AddAddress('grace@chavezlawncare.com');
-    $mail->Username="chavezlawncare22@gmail.com";
-    $mail->Password="8Bc9ZZ15";
-    $mail->SetFrom('no-reply@chavezlawncare.com', 'Chavez LawnCare');
-    $mail->Subject    = $email_subject;
-    $mail->MsgHTML($email_body);
+    //Send mail and retrieve value of success or fail
+    $result = mail("manny.chavez12@yahoo.com", $email_subject, $email_body, $headers);
 
-    if(!$mail->send()) {
+    if(!$result) {
         echo 'Message could not be sent.';
-        echo 'Mailer Error: ' . $mail->ErrorInfo;
         return false;
     }
     else {
